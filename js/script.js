8e28e9f4af9ffc2f416f6e6018c9aa69afd058a9
Andrey Malyar <malyar.andrey@list.ru>
2025-01-22 02:22:13 +0500

    feat: passanger menu html css js

--- js/script.js
+++ js/script.js
@@ -121,34 +121,162 @@ panelBtn.forEach(item => {
 /* --------- END accordion footer --------- */
 
 /* --------- Order menu airTickets (пассажиры класс) --------- */
-const airTicketsSeater = document.getElementById("airTicketsSeater");
+// video 1ч 20мин
+const airTicketsPassenger = document.getElementById("airTicketsPassenger");
 const airTicketsMenu = document.getElementById("airTicketsMenu");
 const airTicketsLabel = document.getElementById("airTicketsLabel");
+const selectPassenger = document.getElementById("outSelectedPassenger");
+
+const classBtnParent = document.querySelector(".airTickets-menu-class__btnBox");
+let classBtnLabel = null;
+
+const closeMenuBtn = document.querySelector(".airTickets-menu__closeBtn");
+
 let show = true;
-
-airTicketsSeater.addEventListener("click", (evt) => {
-    evt.stopPropagation()
-
-    if (show) {
-        airTicketsMenu.classList.add("airTickets-seaterBox__menu_active");
-        airTicketsLabel.classList.add("airTickets-seaterBox__label_active");
-    } else {
-        airTicketsMenu.classList.remove("airTickets-seaterBox__menu_active");
-        airTicketsLabel.classList.remove("airTickets-seaterBox__label_active");
-    }
-    show = !show;
-})
-
-const decrementsBtn = document.querySelectorAll(".decrement");
-const incrementsBtn = document.querySelectorAll(".increment");
-const counters = document.querySelectorAll(".counter");
 
 const passengers = {
     adults: 0,
     children: 0,
     babies: 0,
 }
-// video 34мин
-incrementsBtn.forEach(btn => {
-    btn.addEventListener("click", (e) => {})
+
+
+airTicketsPassenger.addEventListener("click", (evt) => {
+    if (show) {
+        // airTicketsMenu.classList.add("airTickets-passengerBox__menu_active");
+        airTicketsMenu.style.height = airTicketsMenu.scrollHeight + "px";
+    } else {
+        // airTicketsMenu.classList.remove("airTickets-passengerBox__menu_active");
+        airTicketsLabel.classList.remove("airTickets-passengerBox__label_active");
+        airTicketsMenu.style.height = "";
+        showSelectPassenger();
+    }
+    show = !show;
 })
+
+airTicketsMenu.addEventListener("click", (evt) => {
+    evt.stopPropagation();
+})
+
+classBtnParent.addEventListener("click", onClickClassBtn);
+
+function onClickClassBtn(evt) {
+    if(evt.target.tagName === "BUTTON" && evt.target !== evt.currentTarget){
+        evt.stopPropagation();
+        const allClassBtn = classBtnParent.querySelectorAll(".airTickets-menu-class__btn");
+        allClassBtn.forEach(btn => {
+            btn.classList.remove("airTickets-menu-class__btn_active");
+        })
+        evt.target.classList.add("airTickets-menu-class__btn_active");
+        classBtnLabel = evt.target.dataset.name.toLowerCase();
+
+    }
+
+    document.removeEventListener("click", onClickClassBtn);
+}
+
+closeMenuBtn.addEventListener("click", (evt) => {
+    airTicketsMenu.style.height = "";
+    showSelectPassenger();
+    // evt.stopPropagation();
+});
+
+function showSelectPassenger(){
+    let titlePassenger = null;
+    const allPassenger = Object.values(passengers).reduce((acc, item) => acc + item);
+    if(!allPassenger) {
+        selectPassenger.textContent = "";
+        airTicketsLabel.classList.remove("airTickets-passengerBox__label_active");
+        return;
+    }
+    airTicketsLabel.classList.add("airTickets-passengerBox__label_active");
+
+    if(allPassenger === 1) { titlePassenger = "пассажир"; }
+    else if(allPassenger >= 2 && allPassenger <= 4){ titlePassenger = "пассажира"; }
+    else { titlePassenger = "пассажиров"; }
+
+
+    selectPassenger.textContent = `${allPassenger} ${titlePassenger}, ${classBtnLabel ? classBtnLabel : "любой"}`;
+
+}
+
+
+
+const decrementsBtn = document.querySelectorAll(".decrement");
+const incrementsBtn = document.querySelectorAll(".increment");
+const counters = document.querySelectorAll(".counter");
+
+
+
+decrementsBtn.forEach(btn => {
+    changeCounter(btn, "dec");
+
+})
+
+incrementsBtn.forEach(btn => {
+    changeCounter(btn, "inc");
+})
+
+
+function changeCounter(btn, sign){
+    let inc = true;
+    if(sign === "dec"){
+        inc = false;
+    }
+
+    btn.addEventListener("click", (evt) => {
+        let id = evt.target.dataset.id;
+
+        switch (id) {
+            case "adults":
+                if(!checkPassengers("adults", inc, evt)) return;
+                inc ? passengers.adults++ : passengers.adults--;
+                counters[0].textContent = passengers.adults;
+                break;
+            case "children":
+                if(!checkPassengers("children", inc, evt)) return;
+                inc ? passengers.children++ : passengers.children--;
+                counters[1].textContent = passengers.children;
+                break;
+            case "babies":
+                if(!checkPassengers("babies", inc, evt)) return;
+                inc ? passengers.babies++ : passengers.babies--;
+                counters[2].textContent = passengers.babies;
+                break;
+        }
+    })
+}
+
+function checkPassengers(key, inc, evt){
+    const allPassengers = Object.values(passengers).reduce((acc, item) => acc + item);
+
+    if(passengers[key] === 0 && !inc) {
+        evt.target.closest(".airTickets-menu__item").querySelector(".decrement").setAttribute("disabled", "");
+        return false;
+    } else if(allPassengers >= 9 && inc) {
+        evt.target.closest(".airTickets-menu__item").querySelector(".increment").setAttribute("disabled", "");
+        incrementsBtn.forEach((item, i) => {
+            item.setAttribute("disabled", "");
+        })
+        return false;
+    }
+
+
+    if(allPassengers >= 0){
+        evt.target.closest(".airTickets-menu__item").querySelector(".decrement").removeAttribute("disabled");
+    } else if(allPassengers <= 9){
+        evt.target.closest(".airTickets-menu__item").querySelector(".increment").removeAttribute("disabled");
+    }
+
+    if(allPassengers <= 9){
+        incrementsBtn.forEach((item, i) => {
+            item.removeAttribute("disabled");
+        })
+    }
+
+    return true;
+}
+/* --------- end Order menu airTickets (пассажиры класс) --------- */
+
+
+
